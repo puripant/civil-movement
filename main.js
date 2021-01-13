b3010a954c2d3c415102ce7cad892d5ed94f297d
Puripant <puripant@gmail.com>
2021-01-13 17:03:32 +0700

    Update data, clean up code, change stem color, update how nodes are highlighted

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-// // Get URL parameters
-// const queryStringRegEx = query => new RegExp(`[?|&]${query}=([^&]*)`);
-// const url = window.location.search;
-// let matches = url.match(queryStringRegEx("data"));
-// const data_group = matches ? matches[1] : "คณะราษฎร";
-
 const width = document.getElementsByTagName('body')[0].offsetWidth;
 const height = document.getElementsByTagName('body')[0].offsetHeight;
 
@@ -37,10 +31,6 @@ const drag = simulation => {
 
 const node_radius = 5;
 const link_length = 10;
-
-// const volume_to_charge_strength = d3.scalePow().exponent(2)
-//   .domain([0, 1])
-//   .range([-0.1, -1000])
 
 const color_player = d3.scaleOrdinal(d3.range(1, 5), [`#FFFFFF`, `#F5FFE0`, `#FF7A00`, `#1A171B`])
   .unknown(`#00ff00`);
@@ -86,13 +76,14 @@ svg.append("defs")
 let node_sizes = {};
 const radius_from_id = id => Math.sqrt(node_sizes[id]);
 
-d3.csv(`data/[ELECT] Civil Movement Data - event_all.csv`).then(data => {
+d3.csv(`[ELECT] Civil Movement Data - event_all.csv`).then(data => {
   let nodes = [];
   let links = [];
   let stems = [];
   let stem_ids = [];
   let stem_nodes;
 
+  data.sort((a, b) => thai_date_from_string(a.date) - thai_date_from_string(b.date))
   data.forEach((d, i) => {
     let id = d.event_no.trim();
     let date = thai_date_from_string(d.date)
@@ -118,7 +109,7 @@ d3.csv(`data/[ELECT] Civil Movement Data - event_all.csv`).then(data => {
     }
 
     if (+d.time_show === 1 || +d.time_show === 2) { // 1 for long line, 2 for short line
-      stems.push({ source: { x: time_x(date), y: height/2 }, target_id: id });
+      stems.push({ source: { x: time_x(date), y: height/2 }, target_id: id, type: +d.player });
       stem_ids.push(id);
     }
     stem_nodes = nodes.filter(d => stem_ids.includes(d.id)) // shallow copy
@@ -155,7 +146,8 @@ d3.csv(`data/[ELECT] Civil Movement Data - event_all.csv`).then(data => {
     .join("path")
       .attr("fill", "none")
       .attr("stroke-width", node_radius/4)
-      .attr("stroke", "black");
+      .attr("stroke", d => color_player(d.type))
+      // .attr("stroke", "black");
   
   const node = svg.append("g")
     .selectAll("circle")
@@ -170,7 +162,17 @@ d3.csv(`data/[ELECT] Civil Movement Data - event_all.csv`).then(data => {
       .call(drag(simulation))
     .on("mouseover", (event, d) => {
       // console.log(d);
-      tooltip.text(`${d.id}: ${d.name} (${thai_date_to_string(d.date)})`);
+      // tooltip.text(`${d.id}: ${d.name} (${thai_date_to_string(d.date)})`);
+      link.attr("opacity", 0.5)
+      stem.attr("opacity", 0.5)
+      node.attr("opacity", 0.5)
+      d3.select(event.currentTarget).attr("opacity", 1)
+    })
+    .on("mouseout", (event, d) => {
+      link.attr("opacity", 1)
+      link.attr("opacity", 1)
+      stem.attr("opacity", 1)
+      node.attr("opacity", 1)
     });
 
   // node.transition()
@@ -181,7 +183,7 @@ d3.csv(`data/[ELECT] Civil Movement Data - event_all.csv`).then(data => {
   //     return t => i(t);
   //   });
   node.transition()
-    .delay((d, i) => i * 20)
+    .delay((d, i) => i*20) //time_x(d.date)*100)
     .duration(2000)
     .attr("r", node_radius);
   
@@ -195,11 +197,6 @@ d3.csv(`data/[ELECT] Civil Movement Data - event_all.csv`).then(data => {
         let m = this.getPointAtLength(this.getTotalLength() - node_radius);
         return `M${bound_x(d.source.x)},${bound_y(d.source.y)} L${bound_x(m.x, false)},${bound_y(m.y, false)}`;
       });
-    // link
-    //   .attr("x1", d => d.source.x + width/2)
-    //   .attr("y1", d => d.source.y + height/2)
-    //   .attr("x2", d => d.target.x + width/2)
-    //   .attr("y2", d => d.target.y + height/2);
 
     for (let i = 0; i < stems.length; i++) {
       let n = stem_nodes.find(d => d.id === stems[i].target_id)
@@ -218,77 +215,3 @@ d3.csv(`data/[ELECT] Civil Movement Data - event_all.csv`).then(data => {
       .attr("cy", d => bound_y(d.y))
   });
 });
-
-// // Audio
-// window.AudioContext = window.AudioContext || window.webkitAudioContext;
-// let context = new AudioContext();
-
-// if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-//   navigator.mediaDevices.getUserMedia({ audio: true })
-//   .then((stream) => {
-//     let mediaStreamSource = context.createMediaStreamSource(stream);
-//     let meter = createAudioMeter(context);
-//     mediaStreamSource.connect(meter);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-// }
-
-// function createAudioMeter(audioContext, clipLevel, averaging, clipLag) {
-//   const processor = audioContext.createScriptProcessor(512)
-//   processor.onaudioprocess = volumeAudioProcess
-//   processor.clipping = false
-//   processor.lastClip = 0
-//   processor.volume = 0
-//   processor.clipLevel = clipLevel || 0.98
-//   processor.averaging = averaging || 0.95
-//   processor.clipLag = clipLag || 750
-
-//   processor.connect(audioContext.destination)
-
-//   processor.checkClipping = function () {
-//     if (!this.clipping) {
-//       return false
-//     }
-//     if ((this.lastClip + this.clipLag) < window.performance.now()) {
-//       this.clipping = false
-//     }
-//     return this.clipping
-//   }
-
-//   processor.shutdown = function () {
-//     this.disconnect()
-//     this.onaudioprocess = null
-//   }
-
-//   return processor
-// }
-
-// function volumeAudioProcess(event) {
-//   const buf = event.inputBuffer.getChannelData(0);
-//   const bufLength = buf.length;
-  
-//   let sum = 0;
-//   for (var i = 0; i < bufLength; i++) {
-//     let x = buf[i]
-//     if (Math.abs(x) >= this.clipLevel) {
-//       this.clipping = true
-//       this.lastClip = window.performance.now()
-//     }
-//     sum += x * x
-//   }
-//   const rms = Math.sqrt(sum / bufLength)
-//   this.volume = Math.max(rms, this.volume * this.averaging)
-  
-//   if (Math.abs(charge_strength - volume_to_charge_strength(this.volume)) > 10) {
-//     charge_strength = volume_to_charge_strength(this.volume);
-//     simulation
-//       .force("link", force_link)
-//       .force("charge", d3.forceManyBody()
-//         .strength(charge_strength)
-//       )
-//     simulation.alphaTarget(0.1).restart();
-//     console.log("changed")
-//   }
-// }
